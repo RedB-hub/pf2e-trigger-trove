e0a1243e18be7df5efc23129f8cfc49b3c47604c
pedrogrullada <pedrogrullada@gmail.com>
2026-07-24 23:45:14 -0400

    Register triggers from opposite system when anachronism is enabled

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,8 +1,16 @@
 import { setupAPI } from "./api.js";
 
 Hooks.once("triggerEngine.registerTriggers", (registerTriggers) => {
+  const primarySystem = game.system.id;
+  const secondarySystem = primarySystem === "pf2e" ? "sf2e" : "pf2e";
+  const isAnachronismActive = game.modules.get(`${secondarySystem}-anachronism`)?.active;
+  
   registerTriggers("trigger-engine", "pf2e-trigger", "modules/pf2e-trigger-trove/triggers/base-triggers.json");
-  registerTriggers("trigger-engine", "pf2e-trigger", `modules/pf2e-trigger-trove/triggers/${game.system.id}-triggers.json`);
+  registerTriggers("trigger-engine", "pf2e-trigger", `modules/pf2e-trigger-trove/triggers/${primarySystem}-triggers.json`);
+  
+  if (isAnachronismActive) {
+    registerTriggers("trigger-engine", "pf2e-trigger", `modules/pf2e-trigger-trove/triggers/${secondarySystem}-triggers.json`);
+  };
 });
 
 Hooks.once("ready", function() {
